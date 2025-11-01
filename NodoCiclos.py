0149e1863a503b0b4f9d94c3145f0386211bc790
l24590411-blip <l24590411@sjuanrio.tecnm.mx>
2025-10-31 21:22:50 -0600

    Add files via upload

---
+++ NodoCiclos.py
@@ -0,0 +1,80 @@
+#Paola de Jesus Morales Andrade
+#Estructura de datos Gpo C
+class Nodo():
+    dato = None
+    siguiente = None
+
+    def __init__(self, dato):
+        self.dato = dato
+        self.siguiente = None
+
+def agregar_al_final(nodo_inicial, dato):
+    nuevo_nodo = Nodo(dato)
+    if nodo_inicial == None:
+        nodo_inicial = nuevo_nodo
+        return nodo_inicial
+    temporal = nodo_inicial
+    while temporal.siguiente:
+        temporal = temporal.siguiente
+    temporal.siguiente = nuevo_nodo
+    return nodo_inicial
+
+def agregar_al_inicio(nodo_inicial, dato):
+    nuevo_nodo = Nodo(dato)
+    nuevo_nodo.siguiente = nodo_inicial
+    return nuevo_nodo
+
+def imprimir_lista(nodo):
+    while nodo != None:
+        print(f"Tenemos {nodo.dato}")
+        nodo = nodo.siguiente
+
+def obtener_cabeza(nodo_inicial):
+    return nodo_inicial
+
+def obtener_cola(nodo_inicial):
+    temporal = nodo_inicial
+    while temporal.siguiente:
+        temporal = temporal.siguiente
+    return temporal
+
+def existe(nodo, busqueda):
+    while nodo != None:
+        if nodo.dato == busqueda:
+            return True
+        nodo = nodo.siguiente
+    return False
+
+def eliminar(nodo, busqueda):
+    if nodo == None:
+        return
+    if nodo.dato == busqueda:
+        return nodo.siguiente
+    temporal = nodo
+    while temporal.siguiente != None:
+        if temporal.siguiente.dato == busqueda:
+            if temporal.siguiente.siguiente != None:
+                temporal.siguiente = temporal.siguiente.siguiente
+            else:
+                temporal.siguiente = None
+                return nodo
+        temporal = temporal.siguiente
+    return nodo
+    
+
+def main():
+    lista = None
+    lista = agregar_al_final(lista, "Luis")
+    lista = agregar_al_final(lista, "Leon")
+    lista = agregar_al_inicio(lista, "Link")
+    print("Antes de eliminar: ")
+    imprimir_lista(lista)
+    lista = eliminar(lista, "Link")
+    print("Después de eliminar: ")
+    imprimir_lista(lista)
+    print(existe(lista, "Link"))
+    print(existe(lista, "Luis"))
+    print(obtener_cabeza(lista).dato)
+    print(obtener_cola(lista).dato)
+    
+main()
